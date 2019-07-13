08fb027f6ec08565c104daabb29554f38e00e1cd
pundrusrivalli17 <52738038+pundrusrivalli17@users.noreply.github.com>
2019-07-13 15:47:35 +0530

    Add files via upload

---
+++ homework python.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## PRIME NUMBER
+
+# In[3]:
+
+
+n = int(input("enter a number: "))
+c = 0
+if i in range(1,n+1):
+    if n%i==0:
+        c=c+1
+if c==2:
+    print("prime")
+else:
+    print("not prime")
+
+
+# In[ ]:
+
+
+
+
